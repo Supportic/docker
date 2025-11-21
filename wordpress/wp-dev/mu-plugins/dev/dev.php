0ad81b88f10e1c948d05e45d26287854d41efb49
Alexander Senger <senger.alexander@yahoo.de>
2025-11-21 14:23:38 +0100

    WordPress restructure dev mu-plugin

--- wordpress/wp-dev/mu-plugins/dev/dev.php
+++ wordpress/wp-dev/mu-plugins/dev/dev.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 // Exit if accessed directly outside WordPress context.
 defined('ABSPATH') || exit;
 
-include_once __DIR__.'/disable-core-updates.php';
-include_once __DIR__.'/redirect-logged-in.php';
-include_once __DIR__.'/auto-login-user-switcher.php';
-include_once __DIR__.'/mailpit.php';
-include_once __DIR__.'/qof.php';
-include_once __DIR__.'/qm.php';
+include_once __DIR__.'/tools/disable-core-updates.php';
+include_once __DIR__.'/tools/redirect-logged-in.php';
+include_once __DIR__.'/tools/auto-login-user-switcher.php';
+include_once __DIR__.'/tools/mailpit.php';
+include_once __DIR__.'/tools/qof.php';
+include_once __DIR__.'/tools/qm.php';
